142e4ce7a33f7bc465a16c5ba4b2fa75125706b5
Avan <layouwen@gmail.com>
2023-11-02 12:34:14 +0800

    fix: script path

--- scripts/save-docker-image.js
+++ scripts/save-docker-image.js
@@ -1,7 +1,7 @@
 const { execSync } = require('child_process');
 const pkg = require('../package.json');
 
-execSync(`docker save -o ./images/${pkg.name}-v${pkg.version}.tar ${pkg.name}:v${pkg.version}`, { stdio: 'inherit' });
+execSync(`docker save -o ./docker-images/${pkg.name}-v${pkg.version}.tar ${pkg.name}:v${pkg.version}`, { stdio: 'inherit' });
 execSync(`cd ./docker-images && tar -zvcf ${pkg.name}-v${pkg.version}.tar.gz ${pkg.name}-v${pkg.version}.tar`, {
   stdio: 'inherit',
 });
