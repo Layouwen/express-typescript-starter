4b17cb24db538c50eb7dadf6cfe5b697a76e2a69
Avan <layouwen@gmail.com>
2024-08-08 18:26:45 +0800

    chore: update dep

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -1,8 +1,4 @@
-import { NextFunction, Request, Response } from 'express';
+import { expressHttpLogger } from '@avanlan/logger';
 import { logger } from '../utils';
 
-export const loggerMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  const { method, url, query, body, headers, ip } = req;
-  logger.daily.info(method, url, query, body, headers['user-agent'], headers.host, ip);
-  next();
-};
+export const loggerMiddleware = expressHttpLogger(logger);
